a55dfe65dd0323ed4279b43468b575fa3c8232ee
Javier Carretero <javi_1986@hotmail.com>
2020-05-05 12:34:41 +0100

    Amended key-vault name

--- key-vault.tf
+++ key-vault.tf
@@ -1,7 +1,7 @@
 module "key-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   product                 = "${var.product}"
-  env                     = "${var.env}"
+  env                     = "shared-${var.env}"
   tenant_id               = "${var.tenant_id}"
   object_id               = "${var.jenkins_AAD_objectId}"
   resource_group_name     = "${azurerm_resource_group.rg.name}"
