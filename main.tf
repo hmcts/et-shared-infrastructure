20776989c5afc786b43c2398f90f7e28a3fad2db
Javier Carretero <javi_1986@hotmail.com>
2020-06-05 17:45:01 +0100

    Added terraform version

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ provider "azurerm" {
 }
 
 locals {
-  tags = merge(var.common_tags,
+  tags = "${merge(var.common_tags,
     map("Team Contact", "#ethos-repl-service")
-    )
+    )}"
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
-  location = var.location
-  tags = local.tags
+  location = "${var.location}"
+  tags = "${local.tags}"
 }
