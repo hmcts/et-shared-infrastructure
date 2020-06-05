e7557480c6a871b3836b51a823dbce08c1f18b4d
Javier Carretero <javi_1986@hotmail.com>
2020-06-05 12:16:16 +0100

    clean up code

--- ecm-db.tf
+++ ecm-db.tf
@@ -1,44 +1,44 @@
 module "ecm-database" {
   source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
   product            = "${var.product}-shared-ecm-db"
-  location           = "${var.location}"
-  env                = "${var.env}"
+  location           = var.location
+  env                = var.env
   postgresql_user    = "ecmconsumer"
   database_name      = "ecmconsumer"
   postgresql_version = "11"
   sku_name           = "GP_Gen5_2"
   sku_tier           = "GeneralPurpose"
   storage_mb         = "51200"
-  common_tags        = "${var.common_tags}"
-  subscription       = "${var.subscription}"
+  common_tags        = var.common_tags
+  subscription       = var.subscription
 }
 
 resource "azurerm_key_vault_secret" "ecm-postgres-user" {
   name         = "ecmconsumer-postgres-user"
-  value        = "${module.ecm-database.user_name}"
-  key_vault_id = "${module.key-vault.key_vault_id}"
+  value        = module.ecm-database.user_name
+  key_vault_id = module.key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "ecm-postgres-password" {
   name         = "ecmconsumer-postgres-password"
-  value        = "${module.ecm-database.postgresql_password}"
-  key_vault_id = "${module.key-vault.key_vault_id}"
+  value        = module.ecm-database.postgresql_password
+  key_vault_id = module.key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "ecm-postgres-host" {
   name         = "ecmconsumer-postgres-host"
-  value        = "${module.ecm-database.host_name}"
-  key_vault_id = "${module.key-vault.key_vault_id}"
+  value        = module.ecm-database.host_name
+  key_vault_id = module.key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "ecm-postgres-port" {
   name         = "ecmconsumer-postgres-port"
-  value        = "${module.ecm-database.postgresql_listen_port}"
-  key_vault_id = "${module.key-vault.key_vault_id}"
+  value        = module.ecm-database.postgresql_listen_port
+  key_vault_id = module.key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "ecm-postgres-database" {
   name         = "ecmconsumer-postgres-database"
-  value        = "${module.ecm-database.postgresql_database}"
-  key_vault_id = "${module.key-vault.key_vault_id}"
+  value        = module.ecm-database.postgresql_database
+  key_vault_id = module.key-vault.key_vault_id
 }
--- key-vault.tf
+++ key-vault.tf
@@ -1,13 +1,13 @@
 module "key-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                    = "${var.product}-shared-${var.env}"
-  product                 = "${var.product}"
-  env                     = "${var.env}"
-  tenant_id               = "${var.tenant_id}"
-  object_id               = "${var.jenkins_AAD_objectId}"
-  resource_group_name     = "${azurerm_resource_group.rg.name}"
+  product                 = var.product
+  env                     = var.env
+  tenant_id               = var.tenant_id
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.rg.name
   # dcd_group_ethos_v2 group object ID
   product_group_object_id = "414c132d-5160-42b3-bbff-43a2e1daafcf"
-  common_tags             = "${var.common_tags}"
-  managed_identity_object_id = "${var.managed_identity_object_id}"
+  common_tags             = var.common_tags
+  managed_identity_object_id = var.managed_identity_object_id
 }
