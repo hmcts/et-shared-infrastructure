6ead2aff89dc4a3140b51b34de5a877c640e1401
Javier Carretero <javi_1986@hotmail.com>
2020-06-08 09:41:35 +0100

    Common tags

--- main.tf
+++ main.tf
@@ -2,14 +2,8 @@ provider "azurerm" {
   version = "1.27.0"
 }
 
-locals {
-  tags = "${merge(var.common_tags,
-    map("Team Contact", "#ethos-repl-service")
-    )}"
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
-  location = "${var.location}"
-  tags = "${local.tags}"
+  location = var.location
+  tags = var.common_tags
 }
