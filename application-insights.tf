bba2607c2a00bcf5619cac5a754f9bf4c497a121
Javier Carretero <javi_1986@hotmail.com>
2020-09-09 16:58:37 +0100

    Upgraded azurerm to v2

--- application-insights.tf
+++ application-insights.tf
@@ -2,7 +2,7 @@ resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-${var.env}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  application_type    = var.application_type
+  application_type    = "Web"
   tags                = local.common_tags
 }
 
