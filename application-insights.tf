10772f1880226c952fa4ec720590abd63b8b91d3
Javier Carretero <javi_1986@hotmail.com>
2020-09-21 08:40:15 +0100

    Updated app insights

--- application-insights.tf
+++ application-insights.tf
@@ -2,8 +2,17 @@ resource "azurerm_application_insights" "appinsights" {
   name     = "${var.product}-${var.env}"
   location = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  application_type = "Web"
-  tags = local.common_tags
+  application_type    = "web"
+
+  tags = var.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance
+      application_type,
+    ]
+  }
 }
 
 output "appInsightsInstrumentationKey" {
