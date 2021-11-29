650310e4e3ba9e69539f1fb6850e4407ee965d29
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2021-11-29 09:59:43 +0000

    Add Application Insights key secret

--- application-insights.tf
+++ application-insights.tf
@@ -15,6 +15,8 @@ resource "azurerm_application_insights" "appinsights" {
   }
 }
 
-output "appInsightsInstrumentationKey" {
-  value = azurerm_application_insights.appinsights.instrumentation_key
+resource "azurerm_key_vault_secret" "app_insights_key" {
+  name         = "AppInsightsInstrumentationKey"
+  value        = azurerm_application_insights.appinsights.instrumentation_key
+  key_vault_id = module.et-key-vault.key_vault_id
 }
