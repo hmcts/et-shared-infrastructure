d5ca02fc3b9873918406393c546bdb77c359b4e7
msl8r <72858053+msl8r@users.noreply.github.com>
2022-05-11 15:33:17 +0100

    DTSPO-6371 - upgrade azurerm provider to 3.2.0 (#19)
    * DTSPO-6371 - upgrade azurerm provider to 3.2.0
    
    * DTSPO-6371 - add tf version file
    
    * DTSPO-6371 - set queue branch to one with v3 changes
    
    * DTSPO-6371 - fix map function
    
    * DTSPO-6371 - set new branch with v3 changes
    
    * DTSPO-6371 - mark sensitive outputs
    
    * DTSPO-6371 - remove app insights output as its not needed

--- servicebus.tf
+++ servicebus.tf
@@ -2,8 +2,8 @@ module "servicebus-namespace" {
   providers = {
     azurerm.private_endpoint = azurerm.private_endpoint
   }
-  
-  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+
+  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=DTSPO-6371_remove_provider"
   name                = "${var.product}-${var.env}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
@@ -12,7 +12,7 @@ module "servicebus-namespace" {
 }
 
 module "create-updates-queue" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=DTSPO-6371_azurerm_upgrade"
   name                = "create-updates"
   namespace_name      = module.servicebus-namespace.name
   resource_group_name = azurerm_resource_group.rg.name
@@ -24,7 +24,7 @@ module "create-updates-queue" {
 }
 
 module "update-case-queue" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=DTSPO-6371_azurerm_upgrade"
   name                = "update-case"
   namespace_name      = module.servicebus-namespace.name
   resource_group_name = azurerm_resource_group.rg.name
@@ -37,57 +37,61 @@ module "update-case-queue" {
 
 # region connection strings and other shared queue information as Key Vault secrets
 resource "azurerm_key_vault_secret" "create_updates_queue_send_conn_str" {
-  name      = "create-updates-queue-send-connection-string"
-  value     = module.create-updates-queue.primary_send_connection_string
+  name         = "create-updates-queue-send-connection-string"
+  value        = module.create-updates-queue.primary_send_connection_string
   key_vault_id = module.et-key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "create_updates_queue_listen_conn_str" {
-  name      = "create-updates-queue-listen-connection-string"
-  value     = module.create-updates-queue.primary_listen_connection_string
+  name         = "create-updates-queue-listen-connection-string"
+  value        = module.create-updates-queue.primary_listen_connection_string
   key_vault_id = module.et-key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "create_updates_queue_max_delivery_count" {
-  name      = "create-updates-queue-max-delivery-count"
-  value     = var.queue_max_delivery_count
+  name         = "create-updates-queue-max-delivery-count"
+  value        = var.queue_max_delivery_count
   key_vault_id = module.et-key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "update_case_queue_send_conn_str" {
-  name      = "update-case-queue-send-connection-string"
-  value     = module.update-case-queue.primary_send_connection_string
+  name         = "update-case-queue-send-connection-string"
+  value        = module.update-case-queue.primary_send_connection_string
   key_vault_id = module.et-key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "update_case_queue_listen_conn_str" {
-  name      = "update-case-queue-listen-connection-string"
-  value     = module.update-case-queue.primary_listen_connection_string
+  name         = "update-case-queue-listen-connection-string"
+  value        = module.update-case-queue.primary_listen_connection_string
   key_vault_id = module.et-key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "update_case_queue_max_delivery_count" {
-  name      = "update-case-queue-max-delivery-count"
-  value     = var.queue_max_delivery_count
+  name         = "update-case-queue-max-delivery-count"
+  value        = var.queue_max_delivery_count
   key_vault_id = module.et-key-vault.key_vault_id
 }
 
 # endregion
 
 output "create_updates_queue_primary_listen_connection_string" {
-  value = module.create-updates-queue.primary_listen_connection_string
+  sensitive = true
+  value     = module.create-updates-queue.primary_listen_connection_string
 }
 
 output "create_updates_queue_primary_send_connection_string" {
-  value = module.create-updates-queue.primary_send_connection_string
+  sensitive = true
+  value     = module.create-updates-queue.primary_send_connection_string
 }
 
 output "update_case_queue_primary_listen_connection_string" {
-  value = module.update-case-queue.primary_listen_connection_string
+  sensitive = true
+  value     = module.update-case-queue.primary_listen_connection_string
 }
 
 output "update_case_queue_primary_send_connection_string" {
-  value = module.update-case-queue.primary_send_connection_string
+  sensitive = true
+  value     = module.update-case-queue.primary_send_connection_string
 }
 
 output "queue_max_delivery_count" {
