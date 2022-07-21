353589f1ca2240ea639d86c9e6ca4b39418c459a
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2022-07-21 14:25:27 +0100

    Remove local tags

--- main.tf
+++ main.tf
@@ -2,18 +2,8 @@ provider "azurerm" {
   features {}
 }
 
-locals {
-  tags = merge(var.common_tags,
-    tomap({
-      "environment"  = var.env,
-      "managedBy"    = var.team_name,
-      "Team Contact" = var.team_contact
-    })
-  )
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = var.location
-  tags     = local.tags
+  tags     = var.common_tags
 }
