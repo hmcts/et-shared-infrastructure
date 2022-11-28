11c929829d18fdbd31932fd28b2c78dd490b0b00
hsjhita <harpreet.jhita@justice.gov.uk>
2022-11-28 16:54:26 +0000

    RET-2893: Create Premium Servicebus

--- serivcebus-premium.tf
+++ serivcebus-premium.tf
@@ -1,4 +1,7 @@
 module "servicebus-namespace-premium" {
+  providers = {
+    azurerm.private_endpoint = azurerm.private_endpoint
+  }
   source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
   name                = "${var.product}-${var.env}-premium"
   resource_group_name = azurerm_resource_group.rg.name
