1386ae3c530f8b6299d28c68ab4fc9c502aeaa29
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-04-03 12:21:40 +0100

    RET-2863

---
+++ servicebus-premium.tf
@@ -0,0 +1,106 @@
+module "servicebus-namespace-premium" {
+  providers = {
+    azurerm.private_endpoint = azurerm.private_endpoint
+  }
+  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+  name                = "${var.product}-${var.env}-premium"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  env                 = var.env
+  common_tags         = var.common_tags
+  zone_redundant      = true
+  sku                 = "Premium"
+}
+
+module "create-updates-queue-premium" {
+  source = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+  name   = "create-updates"
+  namespace_name = module.servicebus-namespace-premium.name
+  resource_group_name = azurerm_resource_group.rg.name
+
+  requires_duplicate_detection            = "true"
+  duplicate_detection_history_time_window = "PT59M"
+  lock_duration                           = "PT5M"
+  max_delivery_count                      = var.queue_max_delivery_count
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+}
+
+module "update-case-queue-premium" {
+  source = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+  name   = "update-case"
+  namespace_name = module.servicebus-namespace-premium.name
+  resource_group_name = azurerm_resource_group.rg.name
+
+  requires_duplicate_detection            = "true"
+  duplicate_detection_history_time_window = "PT59M"
+  lock_duration                           = "PT5M"
+  max_delivery_count                      = var.queue_max_delivery_count
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+}
+
+# region connection strings and other shared queue information as Key Vault secrets
+resource "azurerm_key_vault_secret" "create_updates_queue_send_conn_str_prm" {
+  name         = "create-updates-queue-send-connection-string-premium"
+  value        = module.create-updates-queue-premium.primary_send_connection_string
+  key_vault_id = module.et-key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "create_updates_queue_listen_conn_str_prm" {
+  name         = "create-updates-queue-listen-connection-string-premium"
+  value        = module.create-updates-queue-premium.primary_listen_connection_string
+  key_vault_id = module.et-key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "create_updates_queue_max_delivery_count_prm" {
+  name         = "create-updates-queue-max-delivery-count-premium"
+  value        = var.queue_max_delivery_count
+  key_vault_id = module.et-key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "update_case_queue_send_conn_str_prm" {
+  name         = "update-case-queue-send-connection-string-premium"
+  value        = module.update-case-queue-premium.primary_send_connection_string
+  key_vault_id = module.et-key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "update_case_queue_listen_conn_str_prm" {
+  name         = "update-case-queue-listen-connection-string-premium"
+  value        = module.update-case-queue-premium.primary_listen_connection_string
+  key_vault_id = module.et-key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "update_case_queue_max_delivery_count_prm" {
+  name         = "update-case-queue-max-delivery-count-premium"
+  value        = var.queue_max_delivery_count
+  key_vault_id = module.et-key-vault.key_vault_id
+}
+
+# endregion
+
+output "create_updates_queue_primary_listen_connection_string_prm" {
+  sensitive = true
+  value     = module.create-updates-queue-premium.primary_listen_connection_string
+}
+
+output "create_updates_queue_primary_send_connection_string_prm" {
+  sensitive = true
+  value     = module.create-updates-queue-premium.primary_send_connection_string
+}
+
+output "update_case_queue_primary_listen_connection_string_prm" {
+  sensitive = true
+  value     = module.update-case-queue-premium.primary_listen_connection_string
+}
+
+output "update_case_queue_primary_send_connection_string_prm" {
+  sensitive = true
+  value     = module.update-case-queue-premium.primary_send_connection_string
+}
+
+output "queue_max_delivery_count_prm" {
+  value = var.queue_max_delivery_count
+}
