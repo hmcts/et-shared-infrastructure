450d5d361b64c391f57c0630d38c3b903099a7c9
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2024-07-03 11:58:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,4 +6,7 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = var.location
   tags     = var.common_tags
+  lifecycle {
+    ignore_changes = all
+  }
 }
