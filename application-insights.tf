bab875d534f39aac3f94085aa82510db13a05cd2
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2025-12-17 08:15:21 +0000

    Add Azure Key Vault secrets and role assignments for Application Insights

--- application-insights.tf
+++ application-insights.tf
@@ -15,6 +15,8 @@ moved {
   to   = module.application_insights.azurerm_application_insights.this
 }
 
+# Deprecated: API Keys are being retired in March 2026
+# TODO: Remove after all applications migrate to connection string or Azure AD auth
 resource "azurerm_key_vault_secret" "app_insights_key" {
   name         = "AppInsightsInstrumentationKey"
   value        = module.application_insights.instrumentation_key
@@ -25,4 +27,26 @@ resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
   value        = module.application_insights.connection_string
   key_vault_id = module.et-key-vault.key_vault_id
+}
+
+# Workspace ID for Azure AD authenticated queries
+resource "azurerm_key_vault_secret" "app_insights_workspace_id" {
+  name         = "app-insights-workspace-id"
+  value        = module.application_insights.log_analytics_workspace_id.workspace_id
+  key_vault_id = module.et-key-vault.key_vault_id
+}
+
+# Add the role assignment (Step 3) - prod only
+resource "azurerm_role_assignment" "slack_alerts_monitoring_reader" {
+  count                = var.env == "prod" ? 1 : 0
+  scope                = module.application_insights.id
+  role_definition_name = "Monitoring Reader"
+  principal_id         = "MANAGED_IDENTITY_PRINCIPAL_ID" # Get this from the Function App in Azure Portal
+}
+
+resource "azurerm_role_assignment" "slack_alerts_monitoring_reader" {
+  count                = var.env == "prod" ? 1 : 0
+  scope                = module.application_insights.id
+  role_definition_name = "Monitoring Reader"
+  principal_id         = data.azurerm_linux_function_app.slack_alerts[0].identity[0].principal_id
 }
--- main.tf
+++ main.tf
@@ -10,3 +10,9 @@ resource "azurerm_resource_group" "rg" {
     ignore_changes = all
   }
 }
+
+data "azurerm_linux_function_app" "slack_alerts" {
+  count               = var.env == "prod" ? 1 : 0
+  name                = "et-slack-alerts-func"
+  resource_group_name = "et-slack-alerts-rg"
+}
